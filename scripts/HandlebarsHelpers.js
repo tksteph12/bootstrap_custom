a16b3bfe2cd13c9022847da8318efbe98e46e9f9
Stephane TATCHUM <tksteph12@gmail.com>
2013-08-06 17:44:14 +0200

    select box modified

--- scripts/HandlebarsHelpers.js
+++ scripts/HandlebarsHelpers.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
 		};
 		
 	});
+
+	Handlebars.registerHelper('update', function(value, options) {
+		if (this[value] === undefined) {
+			throw " year picker  error:" + value + " value is not defined in your JSON.";
+		}
+		
+		for (var text in this[value].years) {
+		    var val = years[text];
+		    $('<option/>').val(val).text(text).appendTo($('#yearselector'))
+		};
+		
+	});
 });
